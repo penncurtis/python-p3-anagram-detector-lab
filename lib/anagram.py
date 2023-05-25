5012960aeafd4f619e57355d111fc31e2e807e96
Penn Curtis <penn.s.m.c@gmail.com>
2023-05-25 09:42:46 -0400

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word_letter = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letter:
+                match_word_list.append(word)
+
+        return match_word_list
